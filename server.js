6bd1451bdc6406b85aa3cd7b6d3772f3273b85cc
nepa <stefano.nepa@epfl.ch>
2015-10-14 17:25:08 +0200

    [fix3]

--- server.js
+++ server.js
@@ -4,19 +4,15 @@ var port = process.env.PORT || 1337;
 var TelegramBot = require('node-telegram-bot-api');
 var azure = require('azure');
 
-azure.RoleEnvironment.getConfigurationSettings(function (error, settings) {
-    if (!error) {
-        var token = process.env.botkey;
-        var bot = new TelegramBot(token, { polling: true });
-        
-        bot.on('message', function (msg) {
-            var chatId = msg.chat.id;
-            bot.sendMessage(chatId, "hello!");
-        });
-    }
-});
+var token = process.env.botkey;
+var bot = new TelegramBot(token, { polling: true });
 
+bot.on('text', function (msg) {
+    var chatId = msg.chat.id;
+    bot.sendMessage(chatId, "hello!");
+});
+        
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Nepabot2');
+    res.end('Hello Nepabot0.3');
 }).listen(port);
