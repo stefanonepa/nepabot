34866960e649bc45d3c91ae269c85eb80656d535
stefanonepa <stefano.nepa@outlook.com>
2015-10-14 23:12:47 +0200

    [add] timeTable form EPFL

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
-﻿var TelegramBot = require('node-telegram-bot-api');
+﻿var request = require('request');
+var moment = require('moment');
+var TelegramBot = require('node-telegram-bot-api');
 var token = process.env.botkey;
 var bot = new TelegramBot(token, { polling: true });
 
 bot.on('message', function (msg) {
     var chatId = msg.chat.id;
-    bot.sendMessage(chatId, msg.text);
+
+    if (msg.text == '/nextMetroEPFL') {
+
+        request('http://transport.opendata.ch/v1/connections?from=EPFL&to=Lausanne', function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var result = JSON.parse(body);
+                var message2send = '';
+                for (var i = 1; i <= 4; i++) {
+                    var nextMetro = result.connections[i-1].from.departure;
+                    message2send += i + ': ' + moment(nextMetro).fromNow() + '\n';
+                }
+                bot.sendMessage(chatId, message2send );
+            } else {
+                bot.sendMessage(chatId, 'Error getting timetable');
+            }
+        })
+    } else {
+        bot.sendMessage(chatId, msg.text);
+    }
 });
         
         
