2289bc496953d5befb5a582e648fd57bd7f5b21d
nepa <stefano.nepa@epfl.ch>
2015-10-14 17:30:39 +0200

    ...

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Nepabot0.3');
+    res.end('Hello Nepabot0.4');
 }).listen(port);
